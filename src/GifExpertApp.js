d4a384ab8744456f6eeb83af728c210c9bcbc24f
Javier.MS <javift1031@gmail.com>
2022-04-10 18:27:04 -0400

    se quitan ol para ajustar imagen

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -13,14 +13,12 @@ const GifExpertApp = () => {
             <h2 className="app-title">GifExpertApp</h2>
             <AddCategory setCategories={setCategories}/>
             <hr/>
-            <ol>
                 {
                     categories.map((category) => (
                         <GifGrid key={category} 
                         category={category}/>                    
                     ))
                 }
-            </ol>
         </div>
     );
     }
